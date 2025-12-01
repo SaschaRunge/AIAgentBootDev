77ace451eebdd77be903b78920cda71a12ba91f5
SaschaRunge <sascharunge89@yahoo.de>
2025-12-01 09:56:43 +0100

    implements basic argument handling and prompting

--- main.py
+++ main.py
@@ -10,26 +10,31 @@ def main():
     client = genai.Client(api_key=api_key)
 
     user_prompt, options = handle_arguments(sys.argv)
-    call_llm(client, user_prompt, **options)
+    call_llm(client, user_prompt, *options)
 
 def handle_arguments(args):
     if len(args) < 2:
-        print("Error: Usage is main.py 'prompt'.")
-        print(
-                "Valid arguments:" +
-                "   - --verbose"
-            )
-        sys.exit(1)
+        handle_invalid_arguments()
     
     user_prompt = ""
-    verbose = False
+    options = []
     for arg in args[1:]:
         if user_prompt == "" and not starts_with(arg, "--"):
             user_prompt = arg
-        if arg == "--verbose" :
-            verbose = True
+        elif starts_with(arg, "--"):
+            options.append(arg)
+        else:
+            handle_invalid_arguments()
    
-    return user_prompt, {"verbose": verbose}
+    return user_prompt, options
+
+def handle_invalid_arguments():
+        print("Error: Usage is main.py 'prompt' optional-arguments.")
+        print(
+                "Valid arguments:\n" +
+                "   --verbose"
+            )
+        sys.exit(1)
         
 def starts_with(string, starting_characters):
     if(len(string) <= len(starting_characters)):
@@ -37,19 +42,29 @@ def starts_with(string, starting_characters):
     else:
         return string[:2] == starting_characters
     
-def call_llm(client, user_prompt, **kwargs):
+def call_llm(client, user_prompt, *args):
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    
-    print(f"Verbose is: {'verbose' in kwargs}")
-
-    sys.exit()
 
     response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+
+    if ("--verbose" in args):
+        print(
+            "\n\n\n"
+            f"User prompt: {user_prompt}\n"
+            f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+            f"Response tokens: {response.usage_metadata.candidates_token_count}"
+        )
+
+    print(f"\n\n")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    
+    
+
+
+
 
 if __name__ == "__main__":
     main()
